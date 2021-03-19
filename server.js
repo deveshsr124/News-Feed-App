385e761cfb8c9959db5345774f4215a333bdb453
Devesh Srivastava <deveshsr124@gmail.com>
2021-03-19 22:18:11 +0530

    small changes

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ const port=process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server started at ${port}`);
 })
+
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
