f5b983618c161c302b7e5d85d012082146ecb473
Devesh Srivastava <deveshsr124@gmail.com>
2021-03-19 22:19:39 +0530

    small changes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.listen(port, () => {
 })
 
 
-app.all('/*', function(req, res, next) {
+app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+
 });
